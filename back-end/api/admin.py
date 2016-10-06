9e55a669905aae3f878ef628f48ce12b0d66791c
dkupfer <dani.kupfer@tufts.edu>
2016-10-05 21:51:43 -0400

    Volunteer table

--- back-end/api/admin.py
+++ back-end/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from api import models
 
-# Register your models here.
+class VolunteerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'city', 'state',
+                    'years_of_service', 'jacket', 'jacket_size',
+                    'status', 'team_captain')
+
+admin.site.register(models.Volunteer, VolunteerAdmin)
