ddb1c4e31b08cf8efc9cce80252db54810ea2437
Michael Morisi <michaelmorisi@Michaels-MacBook-Pro.local>
2016-12-02 16:12:20 -0500

    Fixed pixel + password issues

--- back-end/interface/urls.py
+++ back-end/interface/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.conf.urls import url, include
 
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
