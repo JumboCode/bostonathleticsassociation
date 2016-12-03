1360409c95c5c7b0ddadef9e8b991e636703aa60
jgrace03 <jusugrace@gmail.com>
2016-12-02 19:44:30 -0500

    front end fun

--- back-end/interface/static/js/main.js
+++ back-end/interface/static/js/main.js
@@ -9,6 +9,7 @@ function add_event() {
 	new_event_string += '<div id = "new"> NEW </div>';
     new_event_string += '<div id = "ev"> EVENT </div>';
 	new_event_string += '<div id = "box">';
+	new_event_string += '<div id = "close" onclick = "close_pop()"> x </div>';
 	new_event_string += '<input type = "text" id = "new_event" placeholder = "New Event"style="border:none; font-size: 3.3rem; width: 100%; text-align: center">';
 	new_event_string += '<hr style="position:relative; top:-10px">';
 	new_event_string += '<div id = "month">'; 
@@ -54,7 +55,8 @@ function add_event() {
     new_event_string += '</div>';
     new_event_string += '<div id = "upload">';
     new_event_string += '<label for="image">';
-    new_event_string += '<input type="file" name="image" id= "image" style="display:none;">';
+    new_event_string += '<input type="file" name = "image" id= "image" class= "file_upload" style="display:none;" onchange="check_file()">';
+
     new_event_string += '<img src= "../static/images/upload icon.png" alt="upload pic" style="">';
     new_event_string += '</label>';
     new_event_string += '</div>';
@@ -80,24 +82,10 @@ function view_past_event() {
 	new_event_string += '<div id = "new"> PAST </div>';
     new_event_string += '<div id = "ev"> EVENT </div>';
 	new_event_string += '<div id = "box">';
-
+	new_event_string += '<div id = "close" onclick = "close_pop()"> x </div>';
 	new_event_string += '<div id = "past_event" style = "text-align: center; font-size: 3.3rem; padding: 11px">' + title + '</div>';
 	new_event_string += '<hr style="position:relative; top:-10px">';
-	new_event_string += '<div id = "month">'; 
-    new_event_string += '<div class="btn-group">';
-    new_event_string += '<div class="past"><span id = "m">' + month +'</span></div>';
-    new_event_string += '</div>';
-    new_event_string += '</div>';
-    new_event_string += '<div id = "day">';
-    new_event_string += '<div class="btn-group">';
-    new_event_string += '<div class="past"><span id = "m">' + day +'</span></div>';
-    new_event_string += '</div>';
-    new_event_string += '</div>';
-    new_event_string += '<div id = "year">';
-    new_event_string += '<div class="btn-group">';
-    new_event_string += '<div class="past"><span id = "m">' + year +'</span></div>';
-    new_event_string += '</div>';
-    new_event_string += '</div>';
+    new_event_string += month + '/' + day + '/' + year;
     new_event_string += '<div id = "upload">';
     new_event_string += '<label for="image">';
     new_event_string += '<input type="file" name="image" id= "image" style="display:none;">';
@@ -119,11 +107,13 @@ function edit_event() {
 	var month = "1";
 	var day = "1"; 
 	var year = "2016";
+	var file_name = "fake.csv";
 
 	new_event_string = '';
 	new_event_string += '<div id = "new"> EDIT </div>';
     new_event_string += '<div id = "ev"> EVENT </div>';
 	new_event_string += '<div id = "box">';
+	new_event_string += '<div id = "close" onclick = "close_pop()"> x </div>';
 	new_event_string += '<input type = "text" id = "new_event" placeholder = "' + title + '"style="border:none; font-size: 3.3rem; width: 100%; text-align: center">';
 	new_event_string += '<hr style="position:relative; top:-10px">';
 	new_event_string += '<div id = "month">'; 
@@ -168,13 +158,11 @@ function edit_event() {
     new_event_string += '</div>';
     new_event_string += '</div>';
     new_event_string += '<div id = "upload">';
-    new_event_string += '<label for="image">';
-    new_event_string += '<input type="file" name="image" id= "image" style="display:none;">';
-    new_event_string += '<img src= "../static/images/upload icon.png" alt="upload pic" style="">';
-    new_event_string += '</label>';
     new_event_string += '</div>';
     new_event_string += '<div id = "up_vol">';
-    new_event_string += 'Upload New Volunteer Profiles';
+    new_event_string += '<span style="margin-right: 20px">' + file_name + '</span>';
+    new_event_string += '<span id="remove_upload" onclick="remove_upload()">x</span>';
+
     new_event_string += '</div>';
     new_event_string += '<div id = "submit" onclick="get_new_event_info()">';
     new_event_string += '<img src= "../static/images/submit.png" alt="submit pic" style="">';
@@ -182,6 +170,35 @@ function edit_event() {
     new_event_string += '</div>';
 
     document.getElementById("right-col").innerHTML = new_event_string;
+}
+
+function check_file() {
+	var file_type = document.getElementById("image").files[0].type;
+	console.log(file_type);
+
+	if (file_type != "text/csv") {
+		alert("file must be of type filename.csv")
+		return;
+	}
+
+	var remove = '<span id="remove_upload" onclick="remove_upload()">x</span>';
+
+	var file_name = '<span style="margin-right: 20px">' + document.getElementById("image").files[0].name + '</span>';
+
+	document.getElementById("up_vol").innerHTML = file_name + remove; 
+}
+
+function remove_upload() {
+
+	var new_event_string = '';
+	new_event_string += '<label for="image">';
+    new_event_string += '<input type="file" name = "image" id= "image" class= "file_upload" style="display:none;" onchange="check_file()">';
+
+    new_event_string += '<img src= "../static/images/upload icon.png" alt="upload pic" style="">';
+    new_event_string += '</label>';
+
+    document.getElementById("upload").innerHTML = new_event_string;
+    document.getElementById("up_vol").innerHTML = "Upload Volunteer Profiles";
 }
 
 function get_new_event_info() {
@@ -204,4 +221,8 @@ function set_day(n) {
 function set_year(n) {
 	document.getElementById("y").innerHTML = n;
 	year = n;
+}
+
+function close_pop() {
+	document.getElementById("right-col").innerHTML = '';
 }
