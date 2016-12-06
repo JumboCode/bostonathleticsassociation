85a2c7ef1b90521952b7a53b5ee19f84d6ef4394
Jenna <jenna.kubiak@tufts.edu>
2016-12-06 01:07:18 -0500

    display volunteers for specific team captain

--- front-end/BAA/www/app.js
+++ front-end/BAA/www/app.js
@@ -27,35 +27,38 @@ angular.module('starter', ['ionic'])
 
 .controller('MyCtrl', function($scope, $ionicPopup, $http) {
       tokenInfo = getToken();
-      console.log(tokenInfo.token);
-      console.log(tokenInfo.volunteers);
-      $http({
-        method: 'GET',
-        url: '/api/volunteers',
-        headers: {
-          'Authorization': 'Token ' + tokenInfo.token
-        }
-      }).then(function successCallback(response) {
-          $scope.items = response.data
-          $scope.data = {
+      console.log(tokenInfo);
+      //console.log(tokenInfo.token);
+      //console.log(tokenInfo.volunteers);
+      //$http({
+        //method: 'GET',
+        //url: '/api/volunteers',
+        //headers: {
+         // 'Authorization': 'Token ' + tokenInfo.token
+        //}
+      //}).then(function successCallback(response) {
+          //$scope.items = response.data
+          $scope.items = tokenInfo.volunteers;
+          //$scope.data = {
+          $scope.volunteers = {
             showDelete: false
           };
           $scope.showPrompt = function(item) {
             var promptPopup = $ionicPopup.prompt({
-                  title: item.name,
-                  template: '<body><div class="row"> <input type="text" placeholder=" add freetext here" ng-model="item.volunteerComment" id="volunteer_comment" style="height:60px;padding-bottom=20px;"></div><div class="row"><div class="col">'+item.email+'<hr style="margin-bottom:0px;"></div></div><div class="row" style="margin-top:0px;"><div class="col" class"label" style="color:gray;">email</div></div><div class="row"><div class="col">'+item.phone+'<hr></div></div><div class="row"><div class="col" class="label" style="color:gray;">phone number</div></div><div class="row"><div class="col col-50">'+item.city+'<hr></div><div class="col col-50">'+item.state+'<hr></div></div><div class="row"><div class="col col-50" class="label" style="color:gray;">city</div><div class="col col-50" class="label" style="color:gray;">state</div></div><div class="row"><div class="col">'+item.years_of_service+'<hr></div></div><div class="row"><div class="col" class="label" style="color:gray;">years of working with BAA</div></div><div class="row"><div class="col col-50">'+item.jacket_size+'<hr></div></div><div class="row"><div class="col col-50" class="label" style="color:gray;">jacket size</div></div></body>'
+                  title: item.volunteer.name,
+                  template: '<body><div class="row"> <input type="text" placeholder=" add freetext here" ng-model="item.volunteerComment" id="volunteer_comment" style="height:60px;padding-bottom=20px;"></div><div class="row"><div class="col">'+item.volunteer.email+'<hr style="margin-bottom:0px;"></div></div><div class="row" style="margin-top:0px;"><div class="col" class"label" style="color:gray;">email</div></div><div class="row"><div class="col">'+item.volunteer.phone+'<hr></div></div><div class="row"><div class="col" class="label" style="color:gray;">phone number</div></div><div class="row"><div class="col col-50">'+item.volunteer.city+'<hr></div><div class="col col-50">'+item.volunteer.state+'<hr></div></div><div class="row"><div class="col col-50" class="label" style="color:gray;">city</div><div class="col col-50" class="label" style="color:gray;">state</div></div><div class="row"><div class="col">'+item.volunteer.years_of_service+'<hr></div></div><div class="row"><div class="col" class="label" style="color:gray;">years of working with BAA</div></div><div class="row"><div class="col col-50">'+item.volunteer.jacket_size+'<hr></div></div><div class="row"><div class="col col-50" class="label" style="color:gray;">jacket size</div></div></body>'
               });
           }
           $scope.changeStatus = function(item){
             // here we will need to call a function to update the status at the current item on the server side 
           }
-          console.log(response.data)
-      }, function errorCallback(response) {
-          console.log('error');
-          console.log(response[1].name);
+          //console.log(response.data)
+      //}, function errorCallback(response) {
+        //  console.log('error');
+          //console.log(response[1].name);
           // called asynchronously if an error occurs
           // or server returns response with an error status.
-      });
+      //});
 });
 
 var token;
