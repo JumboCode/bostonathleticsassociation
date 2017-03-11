d5205a55626711084c8be7a9fbd0d71029e9e8d1
Ian Luo <ian@ianluo.com>
2017-03-11 16:35:59 -0500

    working on deploy configs

--- back-end/BAA/wsgi.py
+++ back-end/BAA/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BAA.settings.common")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BAA.settings.dev")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
