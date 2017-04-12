132cff5429ecea53799ad0f355a49e4f824a52fc
Tati <tatianadoyle@gmail.com>
2017-04-11 22:31:07 -0400

    Trying Django clean() method to sanitize input

--- back-end/api/api_utility_functions.py
+++ back-end/api/api_utility_functions.py
@@ -12,7 +12,6 @@ from django.core import serializers
 import csv
 from django.contrib.auth.models import Group
 from io import TextIOWrapper
-from bleach import Bleach
 
 from .models import *
 from .serializers import VolunteerSerializer, EventSerializer, AttendeeSerializer
@@ -25,8 +24,7 @@ def EventListPost(self, request, *args, **kwargs):
 
     serializer_class = EventSerializer
 
-    bleach = Bleach()
-    req_name = bleach.clean(request.data.__getitem__('name'))
+    req_name = request.data.__getitem__('name').clean()
     req_date = request.data.__getitem__('date')
     req_csv  = request.FILES.get('csv')
 
