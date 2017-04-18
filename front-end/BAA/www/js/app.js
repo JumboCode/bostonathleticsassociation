34800ecb4693be3834738aae86458b413db3c712
Philip Wang <Zijian.Wang@Tufts.edu>
2017-04-17 20:39:39 -0400

    iOS ready to deploy

--- front-end/BAA/www/js/app.js
+++ front-end/BAA/www/js/app.js
@@ -1,32 +1,24 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
+// app.js
+// Javascript that controls the log-in page of the BAA application
+//
+//
 angular.module('starter', ['ionic'])
 
+	// Disregard this area unless you know a lot about cordova
+	// Keyboard management
 	.run(function ($ionicPlatform) {
 		$ionicPlatform.ready(function () {
 			if (window.cordova && window.cordova.plugins.Keyboard) {
-				// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-				// for form inputs)
 				cordova.plugins.Keyboard.show();
-				//cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-				// Don't remove this line unless you know what you are doing. It stops the viewport
-				// from snapping when text inputs are focused. Ionic handles this internally for
-				// a much nicer keyboard experience.
-				//cordova.plugins.Keyboard.disableScroll(true);
-
 			}
 			if (window.StatusBar) {
 				StatusBar.styleDefault();
 			}
 		});
-	})
+	});
 
 var token;
-var domain = "http://localhost:8000/";
+var domain = "http://floating-castle-71814.herokuapp.com/";
 
 function checkCredentials() {
 	var user = document.getElementById("user").value;
@@ -40,17 +32,16 @@ function checkCredentials() {
 	request.onreadystatechange = function () {
 		if (request.readyState == 4 && request.status == 200) {
 			token = JSON.parse(request.responseText)['token'];
-			//console.log(token);
 			var resp = JSON.parse(request.responseText);
-			//console.log(resp);
-			// var attendeeRequest = new XMLHttpRequest();
-			// request.open();
-			var attendees = resp.volunteers;
-			//console.log(attendees);
 
+			// Get the attendee list
+			var attendees = resp.volunteers;
+
+			// Get attendee and token, store them into local storage
 			localStorage.setItem("attendees", JSON.stringify(attendees));
 			localStorage.setItem("token", token);
-			verCheck();
+
+			// Redirect to attendee list page
 			window.location.href = "list.html";
 		}
 
@@ -61,14 +52,4 @@ function checkCredentials() {
 
 	request.send("username=" + user + "&password=" + pass);
 }
-
-
-function verCheck() {
-	var LS = localStorage.getItem('token');
-
-	if (LS == "True") {
-		window.location.href("list.html")
-	}
-}
-
 
